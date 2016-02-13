7eed4c1c671949c0e7ab8b76882226f148dfaf7f
Silvano <silvanopessoa@gmail.com>
2016-02-13 20:28:55 -0200

    Signed-off-by: Silvano <silvanopessoa@gmail.com>

--- src/main/java/br/com/silvanopessoa/MicroservicesBootApplication.java
+++ src/main/java/br/com/silvanopessoa/MicroservicesBootApplication.java
@@ -2,9 +2,16 @@ package br.com.silvanopessoa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MicroservicesBootApplication {
+public class MicroservicesBootApplication extends SpringBootServletInitializer{
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(MicroservicesBootApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MicroservicesBootApplication.class, args);
