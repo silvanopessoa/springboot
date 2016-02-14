672f22f84ede7883827550764df90016881d1b28
Silvano <silvanopessoa@gmail.com>
2016-02-13 23:04:02 -0200

    Signed-off-by: Silvano <silvanopessoa@gmail.com>

--- src/main/java/br/com/silvanopessoa/WhateverIWant.java
+++ src/main/java/br/com/silvanopessoa/WhateverIWant.java
@@ -1,15 +1,18 @@
 package br.com.silvanopessoa;
 
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class WhateverIWant {
 	
-	@RequestMapping("/hi")
-	public @ResponseBody String hiThere(){
-		return "hello world!";
+	@RequestMapping("/hi/{name}")
+	public String hiThere(Map model, @PathVariable String name){
+		model.put("name", name);
+		return "hello";
 	}
 
 }
